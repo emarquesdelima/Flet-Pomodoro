635620fcb92240b08734a2f84f3225ad6cb9844f
Eduardo Lima <emarquesdelima@gmail.com>
2024-01-09 19:10:56 -0300

    Adjustments to PomodoroModule. It's working, next step is to work in cosmetics

--- pomodoro/modules.py
+++ pomodoro/modules.py
@@ -11,90 +11,87 @@ class PomodoroModule(UserControl):
         self.focus_time = self.settings['focus_period'] * 60
         self.short_break_time = self.settings['short_break'] * 60
         self.long_break_time = self.settings['long_break'] * 60
-        self.current_counter = self.focus_time
+        self.phase_cycle = {
+            0: ['Focus', self.focus_time],
+            1: ['Short Break', self.short_break_time],
+            2: ['Focus', self.focus_time],
+            3: ['Short Break', self.short_break_time],
+            4: ['Focus', self.focus_time],
+            5: ['Long Break', self.long_break_time],
+        }
+        self.current_phase = 0
+        self.current_phase_name = Text(self.phase_cycle[self.current_phase][0])
+        self.current_counter = self.phase_cycle[self.current_phase][1]
+        self.display_mins = Text(divmod(self.current_counter, 60)[0])
+        self.display_secs = Text(divmod(self.current_counter, 60)[1])
         self.is_running = False
-        self.btn_text = 'Start'
+        self.update_ui = True
+        # self.btn_text = 'Start'
+
         super().__init__()
 
-        self.start_stop_btn = OutlinedButton(
-            self.btn_text,
-            on_click=self.toggle_timer
-        )
-
+    # Threading
     def did_mount(self):
-        self.running = True
         self.th = threading.Thread(
-            target=self.counter_update,
-            args=(),
-            daemon=True
+            target=self.run_timer, args=(), daemon=True
         )
         self.th.start()
 
     def will_unmount(self):
         self.running = False
 
-    def toggle_timer(self, e):
-        self.is_running = not self.is_running  # Toggle the running state
-        if self.is_running:
-            e.control.text = "Stop"
-            if not hasattr(self, 'th') or not self.th.is_alive():
-                # Start the thread only if it hasn't been started or if it's not alive
-                self.th = threading.Thread(
-                    target=self.counter_update, daemon=True)
-                self.th.start()
-        else:
-            e.control.text = "Start"
-        self.update()
-        print(e)
-
-    def counter_update(self):
-        counter = self.current_counter
-        while counter > 0 and self.is_running:
-            mins, secs = divmod(self.time, 60)
-            self.min_display.value = "{:02d}".format(mins)
-            self.sec_display.value = "{:02d}".format(secs)
-            self.update()
-            time.sleep(1)
-            counter -= 1
-
-    def counter_module(self):
-        mins, secs = divmod(self.current_counter, 60)
-        self.min_display = Text("{:02d}".format(mins))
-        self.sec_display = Text("{:02d}".format(secs))
-        mounted_display = Column(
+    # UI
+    def TimerDisplay(self):
+        return Row(
             controls=[
-                Row([self.min_display, self.sec_display]),
-                self.start_stop_btn
+                self.display_mins,
+                self.display_secs
             ]
         )
 
-        self.main_display = mounted_display
-        return self.main_display
+    # Logic
+    def switch_phase(self):
+        self.current_phase += 1
+        if self.current_phase < 6:
+            self.current_phase_name.value = self.phase_cycle[self.current_phase][0]
+            self.current_counter = self.phase_cycle[self.current_phase][1]
+            print(f'A - Phase -> {self.current_phase}')
+        elif self.current_phase == 6:
+            print(f'B - Phase -> {self.current_phase}')
+            self.current_phase = 0
+            print(f'C - Phase -> {self.current_phase}')
+            self.current_phase_name.value = self.phase_cycle[self.current_phase][0]
+            self.current_counter = self.phase_cycle[self.current_phase][1]
 
-    def tab(self, text: str):
-        self.focus = Tab(
-            text=text,
-            content=self.counter_module()
-        )
+    def update_timer_display(self):
+        self.display_mins.value, self.display_secs.value = divmod(
+            self.current_counter, 60)
+        self.update()
 
-        return self.focus
+    def run_timer(self):
+        while self.update_ui:
+            if self.is_running:
+                if self.current_counter > 0:
+                    self.current_counter -= 1
+                else:
+                    self.switch_phase()
+                self.update_timer_display()
+            time.sleep(1)
 
-    def pomodoro_tabs(self):
-        self.tabs = Tabs(
-            tab_alignment=TabAlignment.CENTER,
-            indicator_tab_size=True,
-            animation_duration=300,
-            expand=1,
-            tabs=[
-                self.tab(text='Focus'),
-                self.tab(text='Short Break'),
-                self.tab(text='Long Break'),
-            ]
-        )
+    def start_stop_timer(self, e):
+        self.is_running = not self.is_running
+        e.control.text = 'Stop' if self.is_running else 'Start'
+        self.update()
 
     def build(self):
-        return Container(
-            alignment=alignment.center,
-            margin=20,
-            content=self.pomodoro_tabs()
+        start_stop_button = OutlinedButton(
+            'Start',
+            on_click=self.start_stop_timer
+        )
+        return Column(
+            controls=[
+                self.TimerDisplay(),
+                start_stop_button,
+                self.current_phase_name
+            ]
         )
