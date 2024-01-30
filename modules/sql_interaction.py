a7cd90a3b80543e1773e146cdfb4b63863465877
Eduardo Lima <emarquesdelima@gmail.com>
2024-01-30 19:27:36 -0300

    SQL connection module cleaning

--- modules/sql_interaction.py
+++ modules/sql_interaction.py
@@ -1,17 +1,1 @@
-import mysql.connector
-import os
-
-host = "emarques.mysql.pythonanywhere-services.com"
-user = "emarques"
-# Fetching password from environment variable
-password = os.environ.get('DB_PASSWORD')
-db = "emarques$taskito"
-
-# Using mysql.connector to establish a connection
-connection = mysql.connector.connect(
-    host=host, user=user, password=password, database=db)
-
-# Your code here
-
-# Ensure to close the connection
-connection.close()
+# To be implemented
